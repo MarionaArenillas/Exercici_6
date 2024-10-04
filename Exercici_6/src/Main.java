217e73906c23a5a0d6e45a7d213c57e42a5e499a
MarionaArenillas <mariona.arenillas@gracia.lasalle.cat>
2024-10-04 12:21:27 +0200

    He fet l'exercici 6 a Java

---
+++ Exercici_6/src/Main.java
@@ -0,0 +1,24 @@
+//llibreria que utilitzarem per llegir el número demanat a l'usuari
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Benvingut al programa!");
+        Scanner input = new Scanner(System.in);
+
+        double litres;
+        do {
+            //preguntar-li a l'usuari quants litres plou
+            System.out.println("Quants litres per metre quadrat plou? ");
+            litres = input.nextDouble();
+
+            if (litres >= 90) {
+                System.out.println("Obrir les comportes per desviar del riu Onyar al riu Ter.");
+            } else {
+                System.out.println("No obrim les comportes. ");
+            }
+        } while (litres < 90);
+
+    }
+}
